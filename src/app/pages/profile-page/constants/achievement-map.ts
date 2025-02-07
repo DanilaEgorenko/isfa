8499af6362aa7a76a9aae3e5033e6e67976f68ef
EgorenkoDN <egorenkodn@psbank.ru>
2025-02-07 03:15:34 +0300

    feat: add achievs and rating

---
+++ src/app/pages/profile-page/constants/achievement-map.ts
@@ -0,0 +1,7 @@
+export const ACHIEVEMENT_MAP = {
+    login_streak: {
+        logo: "https://static.wikia.nocookie.net/50c557b4-1f2b-48f4-bb44-b4b24eece85a/scale-to-width/370",
+        showValue: true,
+        title: "Сколько дней заходил подряд",
+    },
+};
--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { of } from "rxjs";
+import { ACHIEVEMENT_MAP } from "./constants";
 
 @Component({
     selector: "app-profile-page",
@@ -6,4 +8,21 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
     styleUrls: ["./profile-page.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+    readonly ACHIEVEMENT_MAP = ACHIEVEMENT_MAP;
+
+    profileInfo$ = of({
+        name: "Danila",
+        pic: "https://static.vecteezy.com/system/resources/thumbnails/005/544/718/small_2x/profile-icon-design-free-vector.jpg",
+        status: "Главный тут",
+        last_login: new Date(),
+        rating: 0,
+        achievements: [{ name: "login_streak", value: 1 }],
+        id: 1,
+    });
+
+    getAnimationDuration(value: number): string {
+        const formula = 1 - value / 100;
+        return (formula <= 0.95 ? formula + 0.2 : 0) + "s";
+    }
+}
